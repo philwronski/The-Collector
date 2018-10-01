c78fa89a498a38d8ddcab45822dcf032badba95b
Samuel Marques Antunes <samuel.marques.antunes@devoteam.com>
2018-10-01 11:49:53 +0200

    Add insfrastructure project for persistence

---
+++ infrastructure/src/main/kotlin/fr/philwronski/thecollector/ThecollectorApplication.kt
@@ -0,0 +1,11 @@
+package fr.philwronski.thecollector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThecollectorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThecollectorApplication>(*args)
+}
